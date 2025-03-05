08a5041fb8fd08a386c2347a6cb49847f489afd0
Quang Hung <duongquanghung0122@gmail.com>
2025-03-05 22:53:01 +0700

    build: update the configuration of vite and typescript; adding global color css

--- tests/react-test-utils.tsx
+++ tests/react-test-utils.tsx
@@ -1,6 +1,6 @@
 import type { RenderOptions } from "@testing-library/react";
 import { render } from "@testing-library/react";
-import type { ReactElement } from "react";
+import React, { ReactElement } from "react";
 
 const customRender = (ui: ReactElement, options?: Omit<RenderOptions, "queries">) =>
   render(ui, {
