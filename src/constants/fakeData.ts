98dd90aa51ea998e754f7f43b217804c802ffeef
Quang Hung <duongquanghung0122@gmail.com>
2025-03-08 02:25:28 +0700

    feat: implement home page

---
+++ src/constants/fakeData.ts
@@ -0,0 +1,43 @@
+export const feedbacks = [
+  {
+    name: "Trà Mi",
+    text: "Mình bị Mụn đang theo phác đồ điều trị của bên này và thấy cải thiện hơn thiệt, lúc đầu vẫn còn hoang mang nhưng bạn tư vấn nhiệt tình nên thấy yên tâm hơn, hi vọng da sẽ như mong đợi.",
+  },
+  {
+    name: "Minh Anh",
+    text: "Dịch vụ rất tốt, nhân viên tư vấn cực kỳ nhiệt tình. Sau một thời gian điều trị, da mình cải thiện rõ rệt.",
+  },
+  {
+    name: "Quốc Bảo",
+    text: "Sản phẩm chất lượng, hiệu quả vượt mong đợi. Rất đáng để trải nghiệm!",
+  },
+  {
+    name: "Ngọc Hà",
+    text: "Lần đầu tiên thử nhưng rất hài lòng, sẽ tiếp tục sử dụng dịch vụ ở đây.",
+  },
+  {
+    name: "Phương Linh",
+    text: "Chăm sóc khách hàng rất tốt, tư vấn rõ ràng, kết quả điều trị rất khả quan!",
+  },
+];
+
+export const benefits = [
+  {
+    title: "Hiểu rõ làn da mình với Trắc nghiệm phân tích da",
+    description:
+      "Bạn có chắc là đã hiểu rõ về làn da của mình để chăm sóc tốt nhất chưa? Chúng ta cùng đi sâu tìm hiểu về loại da và các vấn đề mà da bạn đang gặp phải để có phương pháp chăm sóc hiệu quả nhé.",
+    image: "/benefit1.png",
+  },
+  {
+    title: "Xây dựng liệu trình chăm sóc da chuẩn chuyên khoa",
+    description:
+      "Nếu vấn đề quan trọng nhất khi bước vào hành trình chăm sóc da là hiểu rõ về làn da, thì khó khăn tiếp theo chính là lựa chọn đúng phương pháp và các sản phẩm phù hợp. Hãy để các chuyên gia của 7skincare gợi ý những liệu trình chăm sóc da phù hợp với làn da mình.",
+    image: "/benefit2.png",
+  },
+  {
+    title: "Nhận tư vấn và theo dõi của đội ngũ chuyên viên",
+    description:
+      "Nếu bạn vẫn còn nhiều băn khoăn trong hành trình dưỡng da thì đừng lo lắng, đội ngũ chuyên viên và bác sĩ của 7skincare sẽ tư vấn, định hướng và thiết kế liệu trình phù hợp với tình trạng và nhu cầu riêng của làn da mình. Bạn đã sẵn sàng đồng hành cùng 7skincare để thay đổi làn da mình tốt hơn chưa?",
+    image: "/benefit3.png",
+  },
+];
