6a21340fdaa8b336e1b4022206df6f8a659a3556
Quang Hung <duongquanghung0122@gmail.com>
2025-03-09 15:47:47 +0700

    feat: implement AlertProvider for global notifications

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,15 @@ import { BrowserRouter } from "react-router-dom";
 import "./global.css";
 import { Suspense } from "react";
 import AppRouter from "./routes/router";
+import { AlertProvider } from "./context/AlertContext";
 
 const App: React.FC = () => {
   return (
     <Suspense fallback={null}>
       <BrowserRouter>
-        <AppRouter />
+        <AlertProvider>
+          <AppRouter />
+        </AlertProvider>
       </BrowserRouter>
     </Suspense>
   );
