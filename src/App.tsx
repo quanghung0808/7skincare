b297fd6bf9c3ccb1ee39c04c197aa4590a1031d5
Quang Hung <duongquanghung0122@gmail.com>
2025-03-11 23:32:29 +0700

    feat: adding query client provider

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,17 @@ import "./global.css";
 import { Suspense } from "react";
 import AppRouter from "./routes/router";
 import { AlertProvider } from "./context/AlertContext";
+import { QueryClientProvider } from "./context/QueryClientProvider";
 
 const App: React.FC = () => {
   return (
     <Suspense fallback={null}>
       <BrowserRouter>
-        <AlertProvider>
-          <AppRouter />
-        </AlertProvider>
+        <QueryClientProvider>
+          <AlertProvider>
+            <AppRouter />
+          </AlertProvider>
+        </QueryClientProvider>
       </BrowserRouter>
     </Suspense>
   );
