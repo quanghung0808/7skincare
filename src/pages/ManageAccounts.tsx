0a0614dba4521c46afcd6cb83845b00cd33e9877
Quang Hung <duongquanghung0122@gmail.com>
2025-03-14 16:48:14 +0700

    fix: update UI and fix some issues

--- src/pages/ManageAccounts.tsx
+++ src/pages/ManageAccounts.tsx
@@ -1,5 +1,21 @@
+import { Container, Typography, Button, Box, TextField } from "@mui/material";
+
 const ManageAccounts = () => {
-  return <div>Quản lý tài khoản</div>;
+  return (
+    <Container sx={{ marginTop: "20px" }}>
+      <Box display={"flex"} justifyContent={"space-between"} sx={{ marginBottom: "10px" }}>
+        <Typography variant="h5" gutterBottom>
+          Danh sách tài khoản
+        </Typography>
+        <Box display={"flex"} gap={2}>
+          <TextField label="Tìm kiếm tài khoản" variant="outlined" />
+          <Button variant="contained" color="primary">
+            Thêm tài khoản
+          </Button>
+        </Box>
+      </Box>
+    </Container>
+  );
 };
 
 export default ManageAccounts;
